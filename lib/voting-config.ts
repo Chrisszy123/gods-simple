019ef5eea5035b76219f997337fc493862bc0dd2
chrisszy123 <bassygoodluck@gmail.com>
2026-07-02 21:10:39 +0100

    Update voting closure time in voting-config.ts to 20:22 UTC to align with the revised schedule.

--- lib/voting-config.ts
+++ lib/voting-config.ts
@@ -1,7 +1,7 @@
 // Nigeria is WAT = UTC+1
 // 6:00 PM Lagos = 17:00 UTC | 12:00 PM Lagos = 11:00 UTC
 export const VOTING_OPENS_AT  = '2026-07-02T20:12:00.000Z'
-export const VOTING_CLOSES_AT = '2026-07-02T21:22:00.000Z'
+export const VOTING_CLOSES_AT = '2026-07-02T20:22:00.000Z'
 
 export function isVotingOpen(): boolean {
   const now = new Date()
